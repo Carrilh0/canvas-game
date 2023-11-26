ab8ebad0960cc9615476a399f77e0aef9adee142
Vitor Carrilho <vitor.carrilho@homtail.com>
2023-11-26 20:43:25 -0300

    added obstacles

--- index.js
+++ index.js
@@ -5,38 +5,41 @@ function startGame() {
   myLeftBtn = new component(30, 30, "black", 20, 210);
   myRightBtn = new component(30, 30, "black", 80, 210);
 
+  myObstacle = new component(10, 200, "green", 300, 120);
+
   myGameBlue = new component(30, 30, "blue", 10, 120);
 }
 
 const controlHandlers = {
-  mouse: function(e) {
+  mouse: function (e) {
+    myGameArea.canvas.style.cursor = 'none'
     myGameArea.x = e.pageX;
     myGameArea.y = e.pageY;
   },
-  keyDown: function(e) {
+  keyDown: function (e) {
     myGameArea.keys = myGameArea.keys || {};
     myGameArea.keys[e.key] = true;
   },
-  keyUp: function(e) {
+  keyUp: function (e) {
     myGameArea.keys[e.key] = false;
     myGameBlue.speedX = 0;
     myGameBlue.speedY = 0;
   },
-  touch: function(e) {
-    const rect = document.querySelector('canvas').getBoundingClientRect();
+  touch: function (e) {
+    const rect = myGameArea.canvas.getBoundingClientRect();
     const touch = e.touches[0];
     myGameArea.x = touch.clientX - rect.left;
     myGameArea.y = touch.clientY - rect.top;
   },
-  mouseDonwAndTouchStart: function(e) {
+  mouseDonwAndTouchStart: function (e) {
     myGameArea.x = e.pageX;
     myGameArea.y = e.pageY;
   },
-  mouseUpAndMouseEnd:function(e) {
+  mouseUpAndMouseEnd: function (e) {
     myGameArea.x = false;
     myGameArea.y = false;
-  }
-}
+  },
+};
 
 const myGameArea = {
   canvas: document.createElement("canvas"),
@@ -44,12 +47,16 @@ const myGameArea = {
     this.canvas.width = 480;
     this.canvas.height = 270;
     this.context = this.canvas.getContext("2d");
-    this.interval = setInterval(updateGameArea, 20);
+    this.frameNo = 0;
+    this.interval = setInterval(updateGameArea, 10);
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.setControl();
   },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop: function () {
+    clearInterval(this.interval);
   },
   setControl: function (type = "keyboard") {
     myGameArea.control = type;
@@ -57,17 +64,32 @@ const myGameArea = {
     window.removeEventListener("keyup", controlHandlers.keyUp);
     window.removeEventListener("mousemove", controlHandlers.mouse);
     window.removeEventListener("touchmove", controlHandlers.touch);
-    window.removeEventListener("mousedown", controlHandlers.mouseDonwAndTouchStart);
+    window.removeEventListener(
+      "mousedown",
+      controlHandlers.mouseDonwAndTouchStart
+    );
     window.removeEventListener("mouseup", controlHandlers.mouseUpAndMouseEnd);
-    window.removeEventListener("touchstart", controlHandlers.mouseUpAndMouseEnd);
-    window.removeEventListener("touchend", controlHandlers.mouseDonwAndTouchStart);
+    window.removeEventListener(
+      "touchstart",
+      controlHandlers.mouseUpAndMouseEnd
+    );
+    window.removeEventListener(
+      "touchend",
+      controlHandlers.mouseDonwAndTouchStart
+    );
 
     if (type === "touch") {
       window.addEventListener("touchmove", controlHandlers.touch);
     } else if (type == "buttons") {
-      window.addEventListener("mousedown", controlHandlers.mouseDonwAndTouchStart);
+      window.addEventListener(
+        "mousedown",
+        controlHandlers.mouseDonwAndTouchStart
+      );
       window.addEventListener("mouseup", controlHandlers.mouseUpAndMouseEnd);
-      window.addEventListener("touchstart", controlHandlers.mouseDonwAndTouchStart);
+      window.addEventListener(
+        "touchstart",
+        controlHandlers.mouseDonwAndTouchStart
+      );
       window.addEventListener("touchend", controlHandlers.mouseUpAndMouseEnd);
     } else if (type === "keyboard") {
       window.addEventListener("keydown", controlHandlers.keyDown);
@@ -77,6 +99,10 @@ const myGameArea = {
     }
   },
 };
+
+function everyInterval(n) {
+  return (myGameArea.frameNo / n) % 1 == 0 ? true : false
+}
 
 function component(width, height, color, x, y) {
   this.width = width;
@@ -110,6 +136,24 @@ function component(width, height, color, x, y) {
     this.x += this.speedX;
     this.y += this.speedY;
   };
+  this.crashWith = function (otherObject) {
+    let myLeft = this.x;
+    let myRight = this.x + this.width;
+    let myTop = this.y;
+    let myBottom = this.y + this.height;
+
+    var otherLeft = otherObject.x;
+    var otherRight = otherObject.x + otherObject.width;
+    var otherTop = otherObject.y;
+    var otherBottom = otherObject.y + otherObject.height;
+
+    return !(
+      myBottom < otherTop ||
+      myTop > otherBottom ||
+      myRight < otherLeft ||
+      myLeft > otherRight
+    );
+  };
 }
 
 function changeControl(type) {
@@ -127,16 +171,16 @@ function setControl() {
   if (control === "keyboard") {
     // Keyboard
     if (keys && (keys["ArrowUp"] || keys["w"])) {
-      myGameBlue.speedY = -1;
+      myGameBlue.speedY = -2;
     }
     if (keys && (keys["ArrowDown"] || keys["s"])) {
-      myGameBlue.speedY = 1;
+      myGameBlue.speedY = 2;
     }
     if (keys && (keys["ArrowLeft"] || keys["a"])) {
-      myGameBlue.speedX = -1;
+      myGameBlue.speedX = -2;
     }
     if (keys && (keys["ArrowRight"] || keys["d"])) {
-      myGameBlue.speedX = 1;
+      myGameBlue.speedX = 2;
     }
   } else if (control === "mouse" || control === "touch") {
     if (myGameArea.x && myGameArea.y) {
@@ -166,11 +210,44 @@ function setControl() {
   }
 }
 
+let myObstacles = [];
+
 function updateGameArea() {
+  let x, height, gap, minHeight, maxHeight, minGap, maxGap;
+  for (i = 0; i < myObstacles.length; i += 1) {
+    if (myGameBlue.crashWith(myObstacles[i])) {
+      myGameArea.stop();
+      return;
+    }
+  }
+
   myGameArea.clear();
-  setControl()
+  myGameArea.frameNo += 1;
+
+  if (myGameArea.frameNo == 1 || everyInterval(80)) {
+    x = myGameArea.canvas.width;
+    y = myGameArea.canvas.height - 200
+
+    minHeight = 20;
+    maxHeight = 200;
+    height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+
+    minGap = 50;
+    maxGap = 200;
+
+    gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+
+    myObstacles.push(new component(10, height, "green", x, 0));
+    myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
+  }
+  for (i = 0; i < myObstacles.length; i += 1) {
+    myObstacles[i].x += -1;
+    myObstacles[i].update();
+  }
+
   myGameBlue.newPos();
   myGameBlue.update();
+  setControl();
 }
 
 startGame();
